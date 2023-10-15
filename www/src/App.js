984b45d642470731f77cf60589263157ee5b0c6b
IvanSlivenko <ivan8822@ulr.net>
2023-10-15 09:59:38 +0300

    15-10-2023-10-00

---
+++ www/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Header from './components/Header'
+
+class App extends React.Component {
+  helpText = "Help text !";
+  render() {
+    return (
+      <div>
+        <Header title="Шапка Сайту" name="Name site" />
+        <Header title="Шапка Сайту!" />
+        <Header title="!!!" />
+        <h1>{this.helpText}</h1>
+        <input
+          placeholder={this.helpText}
+          onClick={this.inputClick}
+          onMouseEnter={this.mouseOver}
+        />
+        <p>{this.helpText === "Help text !" ? "Yes" : "No"}</p>
+      </div>
+    );
+  }
+
+  inputClick() {
+    console.log("Cliked");
+  }
+  mouseOver() {
+    console.log("Mouse Over");
+  }
+}
+
+export default App
