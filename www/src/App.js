a022c9bf8bca4592490fe40a472880e15ec4472a
IvanSlivenko <ivan8822@ulr.net>
2023-10-16 22:01:09 +0300

    16-10-2023-22-01

--- www/src/App.js
+++ www/src/App.js
@@ -1,25 +1,46 @@
 import React from "react";
 
 import Header from './components/Header'
+import Image from "./components/Image";
+import logo from "./img/logo.png";
 
 class App extends React.Component {
-  helpText = "Help text !";
+  constructor(props) {
+    super(props);
+    this.state = {
+      helpText: "Help text",
+      userData: "",
+    };
+
+    this.inputClick = this.inputClick.bind(this);
+  }
+
+  componentDidUpdate(prevProp) {
+    if (this.state.helpText !== "Help")
+      console.log("Some");
+  }
+
   render() {
     return (
       <div>
         <Header title="Шапка Сайту" name="Name site" />
-        <h1>{this.helpText}</h1>
+        <h1>{this.state.helpText}</h1>
+        <h2>{this.state.userData}</h2>
         <input
-          placeholder={this.helpText}
+          placeholder={this.state.helpText}
+          onChange={(event) => this.setState({ userData: event.target.value })}
           onClick={this.inputClick}
           onMouseEnter={this.mouseOver}
         />
-        <p>{this.helpText === "Help text !" ? "Yes" : "No"}</p>
+        <p>{this.state.helpText === "Help text !" ? "Yes" : "No"}</p>
+        <Image image={logo} />
+        <img src={logo} />
       </div>
     );
   }
 
   inputClick() {
+    this.setState({ helpText: "Cnanged" });
     console.log("Cliked");
   }
   mouseOver() {
